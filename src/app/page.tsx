4684c7dc249aa295c09069eed041b7d65d2db391
Omar Nurbagndov <omarnurbagandov0103@gmail.com>
2024-04-15 23:52:50 +0300

    test 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,39 @@
-import Homepage from '../components/Homepage/Homepage'
+'use client'
+
 import styles from './page.module.css'
 
-export default function Home() {
+function Home() {
+  function notifyMe() {
+    if (!('Notification' in window)) {
+      // Check if the browser supports notifications
+      alert('This browser does not support desktop notification')
+    } else if (Notification.permission === 'granted') {
+      // Check whether notification permissions have already been granted;
+      // if so, create a notification
+      const notification = new Notification('Hi there!')
+      // …
+    } else if (Notification.permission !== 'denied') {
+      // We need to ask the user for permission
+      Notification.requestPermission().then(permission => {
+        // If the user accepts, let's create a notification
+        if (permission === 'granted') {
+          const notification = new Notification('Hi there!')
+          // …
+        }
+      })
+    }
+
+    // At last, if the user has denied notifications, and you
+    // want to be respectful there is no need to bother them anymore.
+  }
+
   return (
     <main className={styles.main}>
-      <Homepage />
+      <button onClick={() => notifyMe()} className={styles.button}>
+        Notification
+      </button>
     </main>
   )
 }
+
+export default Home
