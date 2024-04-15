e01c29f6ab27974f87df46722b713df72c22b60b
Omar Nurbagndov <omarnurbagandov0103@gmail.com>
2024-04-15 23:12:42 +0300

    fixed bugs 2

--- src/components/CreatePhone/package/Package.tsx
+++ src/components/CreatePhone/package/Package.tsx
@@ -1,31 +1,31 @@
 'use client'
 
 import { Context, initialPhoneState, initialPhonePrice } from '@/app/layout'
-import { useLocalStorage } from '@/hooks/useLocalStorage'
+// import { useLocalStorage } from '@/hooks/useLocalStorage'
 import { useContext } from 'react'
 
 export default function Package() {
   const context = useContext<any>(Context)
 
-  const { getItem: localYourPhones, setItem: setLocalYourPhones } =
-    useLocalStorage('yourPhones')
+  // const { getItem: localYourPhones, setItem: setLocalYourPhones } =
+  //   useLocalStorage('yourPhones')
 
-  const createPhone = () => {
-    context.setYourPhone((prev: any) => [context.phone, ...prev])
-    context.setPhone(initialPhoneState)
-    context.setSidebar(1)
-    // context.setPrice(initialPhonePrice)
-    const prev: any = localYourPhones()
+  // const createPhone = () => {
+  //   context.setYourPhone((prev: any) => [context.phone, ...prev])
+  //   context.setPhone(initialPhoneState)
+  //   context.setSidebar(1)
+  //   // context.setPrice(initialPhonePrice)
+  //   const prev: any = localYourPhones()
 
-    setLocalYourPhones(JSON.stringify([context.phone, ...JSON.parse(prev)]))
-  }
+  //   setLocalYourPhones(JSON.stringify([context.phone, ...JSON.parse(prev)]))
+  // }
 
   return (
     <div
       style={{ display: context.sidebar === 8 ? 'flex' : 'none' }}
       className='createPhone_package'
     >
-      <button onClick={() => createPhone()}>Create phone</button>
+      {/* <button onClick={() => createPhone()}>Create phone</button> */}
     </div>
   )
 }
